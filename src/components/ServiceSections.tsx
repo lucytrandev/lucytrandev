392c03c4053e878943b4e893b69ae643c2d2e981
thuhuongtran <huong.tranthu.hust@gmail.com>
2025-04-25 23:24:12 -0400

    Update services style

--- src/components/ServiceSections.tsx
+++ src/components/ServiceSections.tsx
@@ -9,72 +9,62 @@ export default function ServicesSection() {
                     <h2 className="text-xl sm:text-4xl font-extrabold tracking-wide">
                         Services
                     </h2>
-                    <svg
-                        className="w-6 h-6 text-[#cf5b5a]"
-                        fill="none"
-                        stroke="currentColor"
-                        viewBox="0 0 24 24"
-                        strokeWidth={6}
-                    >
-                        {/* Left arrow path */}
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M15 19l-7-7 7-7"/>
-                    </svg>
                 </div>
             </div>
 
             {/* Service Boxes (2 x 2 Grid) */}
             <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
             <div
-                    className="flex items-center gap-4 bg-blue-900/70 text-white p-4 rounded-md shadow hover:shadow-lg transition">
-                    <div className="flex items-center justify-center w-12 h-12 rounded-full bg-white">
+                    className="flex items-center gap-4 bg-[#26344c] text-white p-4 rounded-md shadow hover:shadow-lg transition">
+                    <div className="flex items-center justify-center w-12 h-12 rounded-full bg-[#35435b]">
                         <Image
                             src="/icons/bot.svg"
                             alt="AI/ML Icon"
                             width={24}
                             height={24}
-                            className="text-[#94c1ab]"
+                            className="brightness-0 invert"
                         />
                     </div>
                     <p className="font-semibold">AI Trading Bot</p>
                 </div>
 
                 <div
-                    className="flex items-center gap-4 bg-blue-900/70 text-white p-4 rounded-md shadow hover:shadow-lg transition">
-                    <div className="flex items-center justify-center w-12 h-12 rounded-full bg-white">
+                    className="flex items-center gap-4 bg-[#26344c] text-white p-4 rounded-md shadow hover:shadow-lg transition">
+                    <div className="flex items-center justify-center w-12 h-12 rounded-full bg-[#35435b]">
                         <Image
                             src="/icons/web.svg"
                             alt="AI/ML Icon"
                             width={24}
                             height={24}
-                            className="text-[#94c1ab]"
+                            className="brightness-0 invert"
                         />
                     </div>
                     <p className="font-semibold">Web Development</p>
                 </div>
 
                 <div
-                    className="flex items-center gap-4 bg-blue-900/70 text-white p-4 rounded-md shadow hover:shadow-lg transition">
-                    <div className="flex items-center justify-center w-12 h-12 rounded-full bg-white">
+                    className="flex items-center gap-4 bg-[#26344c] text-white p-4 rounded-md shadow hover:shadow-lg transition">
+                    <div className="flex items-center justify-center w-12 h-12 rounded-full bg-[#35435b]">
                         <Image
                             src="/icons/aiml.svg"
                             alt="AI/ML Icon"
                             width={24}
                             height={24}
-                            className="text-[#94c1ab]"
+                            className="brightness-0 invert"
                         />
                     </div>
                     <p className="font-semibold">AI / Machine Learning</p>
                 </div>
 
                 <div
-                    className="flex items-center gap-4 bg-blue-900/70 text-white p-4 rounded-md shadow hover:shadow-lg transition">
-                    <div className="flex items-center justify-center w-12 h-12 rounded-full bg-white">
+                    className="flex items-center gap-4 bg-[#26344c] text-white p-4 rounded-md shadow hover:shadow-lg transition">
+                    <div className="flex items-center justify-center w-12 h-12 rounded-full bg-[#35435b]">
                         <Image
                             src="/icons/feelance.svg"
                             alt="AI/ML Icon"
                             width={24}
                             height={24}
-                            className="text-[#94c1ab]"
+                            className="brightness-0 invert"
                         />
                     </div>
                     <p className="font-semibold">Freelancing</p>
